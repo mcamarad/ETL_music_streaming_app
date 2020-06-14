e10673b39b173dd3cd162d5986e5a1b054467b5c
Marcos Cámara Donoso <mcd95alm@gmail.com>
2020-06-14 23:40:46 +0200

    Update sql_queries.py. Conflict for users changing plans solved.
    Modified conflict clause for users table to avoid conflicts when users switch between free to paid plans.

--- sql_queries.py
+++ sql_queries.py
@@ -8,7 +8,8 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id SERIAL PRIMARY KEY,
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays 
+                            (songplay_id SERIAL PRIMARY KEY,
                             start_time bigint NOT NULL,
                             user_id int NOT NULL,
                             level varchar NOT NULL,
@@ -55,28 +56,33 @@ time_table_create = ("""CREATE TABLE IF NOT EXISTS time (
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+songplay_table_insert = ("""INSERT INTO songplays 
+(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 ON CONFLICT DO NOTHING
 """)
 
-user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level)
+user_table_insert = ("""INSERT INTO users 
+(user_id, first_name, last_name, gender, level)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT(user_id) DO UPDATE SET level = excluded.level
+""")
+
+song_table_insert = ("""INSERT INTO songs 
+(song_id, title, artist_id, year, duration)
 VALUES (%s, %s, %s, %s, %s)
 ON CONFLICT DO NOTHING
 """)
 
-song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration)
+artist_table_insert = ("""INSERT INTO artists 
+(artist_id, name, location, latitude, longitude)
 VALUES (%s, %s, %s, %s, %s)
 ON CONFLICT DO NOTHING
 """)
 
-artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude)
-VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT DO NOTHING
-""")
 
-
-time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+time_table_insert = ("""INSERT INTO time 
+(start_time, hour, day, week, month, year, weekday)
 VALUES (%s, %s, %s, %s, %s, %s, %s)
 ON CONFLICT DO NOTHING
 """)
@@ -84,7 +90,8 @@ ON CONFLICT DO NOTHING
 # FIND SONGS
 
 song_select = ("""SELECT song_id, artists.artist_id 
-FROM songs JOIN artists ON songs.artist_id=artists.artist_id WHERE title = %s AND name = %s AND duration = %s
+FROM songs JOIN artists ON songs.artist_id=artists.artist_id 
+WHERE title = %s AND name = %s AND duration = %s
 """)
 
 # QUERY LISTS
